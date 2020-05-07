f0ecb804de0fdc154e1e596214036a6b46d9f68d
EricHier <19eric03@gmail.com>
2020-05-07 16:19:01 +0200

    added plausible tracking script

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue';
+import DefaultLayout from "~/layouts/Default.vue";
 import "~/assets/css/main.css";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 
 require("typeface-josefin-sans");
 require("typeface-baloo-paaji");
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout)
+
+  head.script.push({
+    src: "https://plausible.io/js/plausible.js",
+    async: true,
+    defer: true,
+    "data-domain": "erichier.tech"
+  });
 }
