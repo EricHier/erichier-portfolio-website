ce15192da699a36f6b6203775bfc80c829333c71
EricHier <developer.erichier@gmail.com>
2020-09-15 18:50:31 +0200

    rel noopener for ofpage links

--- src/components/projects/ProjectContainer.vue
+++ src/components/projects/ProjectContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href="href" target="_blank" class="block container mx-auto md:grid md:grid-cols-2 gap-4 project-container mb-30" :title="title" draggable="false">
+    <a :href="href" target="_blank" rel="noopener" class="block container mx-auto md:grid md:grid-cols-2 gap-4 project-container mb-30" :title="title" draggable="false">
         <slot/>
     </a>
 </template>
