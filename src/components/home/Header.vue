5686c52e87f406dea6857d3a5ddf7080dce0bf57
EricHier <developer.erichier@gmail.com>
2020-09-20 22:24:43 +0200

    added accent dots in header

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -1,27 +1,30 @@
 <template>
-    <section class="h-screen flex flex-col justify-between font-heading text-white">
-        <div class="container mx-auto p-10" />
+  <section class="h-screen flex flex-col justify-between font-heading text-white">
+    <div class="container mx-auto p-10"/>
 
-        <div class="container mx-auto p-10 flex flex-col md:flex-row justify-between">
-            <div>
-                <h2 class="text-2xl md:text-4xl mb-4" data-aos="fade">
-                    Zu jeder Problemstellung bedarf es einer L&ouml;sung.
-                </h2>
-                <h2 class="text-2xl md:text-4xl" data-aos="fade" data-aos-delay="1000">
-                    Und diese zu entwickeln ist meine Passion.
-                </h2>
-            </div>
-            <h1 class="text-3xl md:text-5xl text-right" data-aos="fade" data-aos-delay="2000">
-                Hi!<br>
-                EricHier
-            </h1>
-        </div>
+    <div class="container mx-auto p-10 flex flex-col md:flex-row justify-between">
+      <div>
+        <h2 class="text-2xl md:text-4xl mb-4" data-aos="fade">
+          Zu jeder Problemstellung bedarf es einer L&ouml;sung.
+        </h2>
+        <h2 class="text-2xl md:text-4xl" data-aos="fade" data-aos-delay="1000">
+          Und diese zu entwickeln ist meine Passion.
+        </h2>
+        <div class="w-32 h-2 border-b-8 border-dotted border-red-500 mt-2"/>
+      </div>
+      <h1 class="text-3xl md:text-5xl text-right" data-aos="fade" data-aos-delay="2000">
+        Hi!<br>
+        EricHier
+      </h1>
+    </div>
 
-        <div class="w-full relative mx-auto h-96px text-center flex justify-center items-center text-2xl md:text-3xl text-black z-10 text-white has-caro-background">
-            <h2>Meine Projekte</h2>
-            <div class="absolute top-0 left-0 flex justify-center items-center w-full h-full bg-white text-black" data-aos="fade" data-aos-anchor="#projects">
-                Meine Projekte
-            </div>
-        </div>
-    </section>
+    <div
+        class="w-full relative mx-auto h-96px text-center flex justify-center items-center text-2xl md:text-3xl text-black z-10 text-white has-caro-background">
+      <h2>Meine Projekte</h2>
+      <div class="absolute top-0 left-0 flex justify-center items-center w-full h-full bg-white text-black"
+           data-aos="fade" data-aos-anchor="#projects">
+        Meine Projekte
+      </div>
+    </div>
+  </section>
 </template>
